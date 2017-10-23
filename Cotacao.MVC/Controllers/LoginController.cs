1716ca14584aad3851606980fcdd930103fbed3e
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-10-23 21:21:22 -0200

    Sistema de login

---
+++ Cotacao.MVC/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Cotacao.Model;
+
+namespace Cotacao.MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Autenticar([FromForm]string Email, [FromForm]string Senha)
+        {
+            if (Usuario.Autenticar(Email,Senha))
+            {
+                return "Logado comm sucesso!";
+            }
+            else
+            {
+                return "Login ou senha inválidos!";
+            }
+        }
+    }
+}
