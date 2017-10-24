244e46253a28731729fefdd9cc6d965934547140
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-10-24 21:37:40 -0200

    Sistema de cadastro de novos usuários

--- Cotacao.MVC/Controllers/LoginController.cs
+++ Cotacao.MVC/Controllers/LoginController.cs
@@ -18,11 +18,46 @@ namespace Cotacao.MVC.Controllers
             if (Usuario.Autenticar(Email, Senha))
             {
                 throw new System.Exception();
+                //TODO: Implementar sistema de controle de acesso em páginas específicas
             }
             else
             {
                 ViewBag.Error = true;
                 ViewData["Title"] = "Login";
+                return View();
+            }
+        }
+
+        public IActionResult Cadastro()
+        {
+            ViewData["Title"] = "Cadastro usuário";
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro([FromForm]string Nome, [FromForm]string Email, [FromForm]string Senha)
+        {
+            try
+            {
+                ViewData["Title"] = "Cadastro usuário";
+
+                Usuario _usuario = new Usuario();
+
+                _usuario.Nome = Nome;
+                _usuario.Email = Email;
+                _usuario.Senha = Senha;
+
+                Usuario.Cadastrar(_usuario);
+
+                return View();
+            }
+            catch (System.Exception e)
+            {
+                ViewData["Title"] = "Cadastro usuário";
+                ViewBag.Error = true;
+                ViewBag.MensageErro = e.Message;
+
                 return View();
             }
         }
